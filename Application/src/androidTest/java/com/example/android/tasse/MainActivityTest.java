060f62acee6892a773eb1f9e89846aa2c8da07f9
Jussi Yli-Urpo <jyliurpo@gmail.com>
2015-08-31 18:38:53 +0300

    Move tests under standard locations. Create/update example tests.
    * Move functional tests under src/androidTest. Create example test for MainActivity.
    * Create example unit test for Stop class under src/test
    * Add junit dependency.

---
+++ Application/src/androidTest/java/com/example/android/tasse/MainActivityTest.java
@@ -0,0 +1,66 @@
+/**
+ * Created by jussi on 27.8.15.
+ */
+package com.example.android.tasse;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
+
+/**
+ * Tests for NetworkConnect sample.
+ */
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    private MainActivitymTestActivity;
+    private EditText mLineIdEditText;
+    private EditText mVehicleIdEditText;
+    private EditText mStopIdEditText;
+    private String title;
+
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mTestActivity = getActivity();
+        mLineIdEditText = (EditText) mTestActivity.findViewById(R.id.lineId);
+        mVehicleIdEditText = (EditText) mTestActivity.findViewById(R.id.vehId);
+        mStopIdEditText = (EditText) mTestActivity.findViewById(R.id.stopId);
+        title = (String) mTestActivity.getActionBar().getTitle().toString();
+    }
+
+    /**
+     * Test if the test fixture has been set up correctly.
+     */
+    public void testPreconditions() {
+        //Try to add a message to add context to your assertions. These messages will be shown if
+        //a tests fails and make it easy to understand why a test failed
+        assertNotNull("mTestActivity is null", mTestActivity);
+        assertNotNull("lineId is null" , mLineIdEditText);
+        assertNotNull("vehId is null", mVehicleIdEditText);
+        assertNotNull("stopId is null", mStopIdEditText);
+    }
+
+    /**
+     * Add more tests below.
+     */
+
+    public void testInitialEditTextValue_lineId() throws Exception {
+        assertEquals("line Id initial value is incorrect", mLineIdEditText.getText().toString(),
+                mTestActivity.getString(R.string.line_id_default).toString());
+    }
+    public void testInitialEditTextValue_vehId() throws Exception {
+        assertEquals("vehicle Id initial value is incorrect", mVehicleIdEditText.getText().toString(),
+                mTestActivity.getString(R.string.vehicle_id_default).toString());
+    }
+    public void testInitialEditTextValue_stopId() throws Exception {
+        assertEquals("stopId initial value is incorrect", mVehicleIdEditText.getText().toString(),
+                mTestActivity.getString(R.string.bus_stop_default).toString());
+    }
+    public void testEditTextHint_stopId() throws Exception {
+        assertEquals("stopId hint is incorrect", mVehicleIdEditText.getHint().toString(),
+                mTestActivity.getString(R.string.bus_stop_hint).toString());
+    }
+}
---
+++ Application/src/test/java/com/example/android/tasse/StopTest.java
@@ -0,0 +1,21 @@
+package com.example.android.tasse;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by jussi on 27.8.15.
+ */
+public class StopTest extends TestCase {
+
+    public void testSetRef() throws Exception {
+
+    }
+
+    public void testGetRef() throws Exception {
+
+    }
+
+    public void testGetId() throws Exception {
+
+    }
+}
